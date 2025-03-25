c78172fef30b27cd536a575c54197f9af4164282
humu <cmt.engr.ariyan@gmail.com>
2025-03-25 18:14:31 +0600

    done

--- 25-mar/index.php
+++ 25-mar/index.php
@@ -28,3 +28,78 @@ $test ->show();
 $test2 = new second();
 $test2 ->show();
 ?>
+
+<!-- abostract class -->
+
+<?php
+
+abstract class parrentClass{
+
+    public $name;
+
+    abstract protected function show($a, $b);
+
+
+}
+
+
+class chield extends parrentClass{
+
+
+    public function show($a, $b){
+        echo $a + $b;
+    }
+
+}
+
+$a1 = new chield();
+$a1->show(10,20);
+
+?>
+
+<!-- interface -->
+
+<?php
+
+interface a{
+    function hallo();
+    function hi();
+}
+interface b{
+    function hallo();
+    function hi();
+}
+interface c{
+    function hallo();
+    function hi();
+}
+interface d{
+    function call();
+    function action();
+}
+
+
+class Child implements a, b, c, d {
+    public function hallo() {
+        echo "Hello from hallo <br>";
+    }
+    
+    public function hi() {
+        echo "Hello from hi <br>";
+    }
+    public function call() {
+        echo "Hello from call <br>";
+    }
+    
+    public function action() {
+        echo "Hello from action <br>";
+    }
+
+}
+
+$at = new child();
+$at->hallo();
+$at->hi();
+$at->call();
+$at->action();
+?>
