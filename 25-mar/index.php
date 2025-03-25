c75e9f32e4d1e78307871672bb4a26ddd3c689b5
humu <cmt.engr.ariyan@gmail.com>
2025-03-25 19:39:04 +0600

    done

--- 25-mar/index.php
+++ 25-mar/index.php
@@ -103,3 +103,84 @@ $at->hi();
 $at->call();
 $at->action();
 ?>
+
+
+<!-- static member -->
+
+<?php
+class parentClass{
+    public static $name = "i am static variable";
+}
+
+class derive extends parentClass{
+
+    public function show(){
+        echo parent::$name;
+    }
+}
+
+$at = new derive();
+$at -> show();
+
+?>
+
+
+<!-- late static binding -->
+
+<?php
+class binding{
+    protected static $name = "i am static variable frome parent class";
+
+    public function show(){
+        echo static::$name;
+    }
+}
+
+class derivebinding extends binding{
+    protected static $name = "i am static variable frome deriver class <br>";
+
+}
+
+$atk = new derivebinding();
+$atk -> show();
+
+?>
+
+<!-- trait -->
+
+<?php
+trait parentTrait{
+    public $name;
+
+
+    public function show($name){
+        echo $name  ;
+    }
+}
+
+class a1 {
+    use parentTrait;
+}
+class a2 {
+    use parentTrait;
+}
+class a3 {
+    use parentTrait;
+}
+class a4 {
+    use parentTrait;
+}
+
+$af1 = new a1();
+$af1->show("this is treit common function from a1 <br>"); 
+
+$ag2 = new a2();
+$ag2->show("this is treit common function from a2 <br>"); 
+
+$ae3 = new a3();
+$ae3->show("this is treit common function from a3 <br>" ); 
+
+$vc4 = new a4();
+$vc4->show("this is treit common function from a4 <br>")
+
+?>
